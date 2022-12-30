36450ee59a758115638b1dbf0a3c4d8f98b6c00e
Lupin <60448443+HellBri8nger@users.noreply.github.com>
2022-12-30 17:34:10 +0530

    a semi-finished version

--- mouse.py
+++ mouse.py
@@ -1,3 +1,86 @@
-import time
+import pydirectinput as pyp
 import keyboard
-import mouse 
+import pyautogui as gui
+import threading
+
+incrementvar = 20
+
+
+class Rightside(threading.Thread):
+    def run(self):
+        while True:
+            if keyboard.is_pressed('6'):
+                pos2 = gui.position()
+                pos = pos2
+                print(pos)
+                xaxis = pos[0] + incrementvar
+                yaxis = pos[1]
+                print(xaxis, yaxis)
+                pyp.moveTo(x=xaxis, y=yaxis)
+
+
+class Downside(threading.Thread):
+    def run(self):
+        while True:
+            if keyboard.is_pressed('2'):
+                pos2 = gui.position()
+                pos = pos2
+                print(pos)
+                yaxis = pos[1] + incrementvar
+                xaxis = pos[0]
+                print(xaxis, yaxis)
+                pyp.moveTo(x=xaxis, y=yaxis)
+
+
+class Leftside(threading.Thread):
+    def run(self):
+        while True:
+            if keyboard.is_pressed('4'):
+                pos2 = gui.position()
+                pos = pos2
+                print(pos)
+                xaxis = pos[0] - incrementvar
+                yaxis = pos[1]
+                print(xaxis, yaxis)
+                pyp.moveTo(x=xaxis, y=yaxis)
+
+
+class Upside(threading.Thread):
+    def run(self):
+        while True:
+            if keyboard.is_pressed('8'):
+                pos2 = gui.position()
+                pos = pos2
+                print(pos)
+                yaxis = pos[1] - incrementvar
+                xaxis = pos[0]
+                print(xaxis, yaxis)
+                pyp.moveTo(x=xaxis, y=yaxis)
+
+
+class click(threading.Thread):
+    def run(selfs):
+        while True:
+            if keyboard.is_pressed('5'):
+                pyp.leftClick()
+
+class rclick(threading.Thread):
+    def run(selfs):
+        while True:
+            if keyboard.is_pressed('0'):
+                pyp.rightClick()
+
+
+moveright = Rightside()
+movedown = Downside()
+moveleft = Leftside()
+moveup = Upside()
+leftclick = click()
+rightclick = rclick()
+
+moveright.start()
+movedown.start()
+moveup.start()
+moveleft.start()
+leftclick.start()
+rightclick.start()
